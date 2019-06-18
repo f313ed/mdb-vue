648476c10fb73499a7a2321a35e3a6f881fe90bb
usuki@go-next.co.jp <f313ed@gmail.com>
2019-06-19 07:05:01 +0900

    add docs & create menu

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,96 @@
 <template>
   <div id="app">
-    <img src="./assets/logo-mdb-vue-small.png">
-    <router-view></router-view>
+
+    <!-- Simple -->
+    <mdb-navbar id="main-navbar" dark position="top" color="purple-gradient" scrolling :scrollingOffset="20">
+      <mdb-navbar-brand to="/" waves class="font-weight-bold">
+        MCG
+      </mdb-navbar-brand>
+      <mdb-navbar-toggler>
+        <mdb-navbar-nav>
+          <mdb-nav-item to="/"><mdb-icon icon="search" size="1x" class="pr-2" /><strong>探す</strong></mdb-nav-item>
+          <mdb-nav-item to="/partner"><mdb-icon icon="thumbs-up" size="1x" class="pr-2" /><strong>お相手から</strong></mdb-nav-item>
+          <mdb-nav-item to="/message"><mdb-icon icon="comment-dots" size="1x" class="pr-2" /><strong>メッセージ</strong></mdb-nav-item>
+          <mdb-nav-item to="/mypage"><mdb-icon icon="user-alt" size="1x" class="pr-2" /><strong>マイページ</strong></mdb-nav-item>
+        </mdb-navbar-nav>
+      </mdb-navbar-toggler>
+    </mdb-navbar>
+
+
+
+    <!-- アイコンver -->
+    <!-- <mdb-navbar  color="aqua-gradient" class="lighten-3 py-4 mb-4">
+      <mdb-navbar-nav nav>
+        <mdb-nav-item to="/" anchorClass="white-text" active><p><mdb-icon icon="search" size="3x" class="amber-text pr-3" aria-hidden="true" />探す</p></mdb-nav-item>
+        <mdb-nav-item to="/casts" anchorClass="white-text"><p><mdb-icon icon="thumbs-up" size="3x" class="indigo-text pr-3" aria-hidden="true" />お相手から</p></mdb-nav-item>
+        <mdb-nav-item to="#" anchorClass="white-text"><p><mdb-icon icon="comment-dots" size="3x" class="pink-text pr-3" aria-hidden="true" />メッセージ</p></mdb-nav-item>
+        <mdb-nav-item to="#" anchorClass="white-text"><p><mdb-icon icon="bell" size="3x" class="yellow-text pr-3" aria-hidden="true" /> お知らせ</p></mdb-nav-item>
+        <mdb-nav-item to="#" anchorClass="white-text"><p><mdb-icon icon="user-alt" size="3x" class="green-text pr-3" aria-hidden="true" /> マイページ</p></mdb-nav-item>
+      </mdb-navbar-nav>
+    </mdb-navbar> -->
+
+    <!-- origin -->
+    <!-- <mdb-navbar id="main-navbar" dark position="top" color="stylish" scrolling :scrollingOffset="20">
+      <mdb-navbar-brand to="/" waves class="font-weight-bold">
+        MDB Vue
+      </mdb-navbar-brand>
+      <mdb-navbar-toggler>
+        <mdb-navbar-nav right>
+          <mdb-nav-item exact to="/"><strong>Home</strong></mdb-nav-item>
+          <mdb-nav-item to="/css"><strong>CSS</strong></mdb-nav-item>
+          <mdb-nav-item to="/components"><strong>Components</strong></mdb-nav-item>
+          <mdb-nav-item to="/advanced"><strong>Advanced</strong></mdb-nav-item>
+          <mdb-nav-item to="/navigation"><strong>Navigation</strong></mdb-nav-item>
+          <mdb-nav-item to="/forms"><strong>Forms</strong></mdb-nav-item>
+          <mdb-nav-item to="/tables"><strong>Tables</strong></mdb-nav-item>
+          <mdb-nav-item to="/modals"><strong>Modals</strong></mdb-nav-item>
+          <mdb-nav-item to="/plugins"><strong>Plugins & addons</strong></mdb-nav-item>
+        </mdb-navbar-nav>
+      </mdb-navbar-toggler>
+    </mdb-navbar> -->
+
+    <!-- 
+    <p><mdb-icon icon="search" size="3x" class="amber-text pr-3" aria-hidden="true" /> amber-text</p>
+    <p><mdb-icon icon="thumbs-up" size="3x" class="red-text pr-3" aria-hidden="true" /> red-text</p>
+    <p><mdb-icon icon="comment-dots" size="3x" class="indigo-text pr-3" aria-hidden="true" /> indigo-text</p>
+    <p><mdb-icon icon="bell" size="3x" class="cyan-text pr-3" aria-hidden="true" /> cyan-text</p>
+    <p><mdb-icon icon="user-alt" size="3x" class="green-text pr-3" aria-hidden="true" /> green-text</p>
+    
+    <p><mdb-icon icon="camera-retro" size="5x" /> fa-camera-retro</p>
+    <p><mdb-icon icon="bolt" size="3x" class="amber-text pr-3" aria-hidden="true" /> amber-text</p>
+    <p><mdb-icon icon="bug" size="3x" class="red-text pr-3" aria-hidden="true" /> red-text</p>
+    <p><mdb-icon icon="heart" size="3x" class="indigo-text pr-3" aria-hidden="true" /> indigo-text</p>
+    <p><mdb-icon icon="bell" size="3x" class="cyan-text pr-3" aria-hidden="true" /> cyan-text</p>
+    <p><mdb-icon icon="birthday-cake" size="3x" class="green-text pr-3" aria-hidden="true" /> green-text</p>
+    -->
+
+    <!-- <mdb-navbar  color="aqua-gradient" class="lighten-3 py-4 mb-4">
+      <mdb-navbar-nav nav class="font-weight-bold">
+        <mdb-nav-item href="#" active anchorClass="white-text">Active</mdb-nav-item>
+        <mdb-nav-item href="#" anchorClass="white-text">Link</mdb-nav-item>
+        <mdb-nav-item href="#" anchorClass="white-text">Link</mdb-nav-item>
+        <mdb-nav-item href="#" anchorClass="white-text">Link</mdb-nav-item>
+      </mdb-navbar-nav>
+    </mdb-navbar> -->
+
+
+    <main :style="{marginTop: '10px'}">
+      <transition name="fade" mode="out-in">
+        <router-view></router-view>
+      </transition>
+    </main>
+    <mdb-footer color="stylish-color">
+      <p class="footer-copyright mb-0 py-3 text-center">
+        &copy; {{new Date().getFullYear()}} Copyright: <a href="https://mdbootstrap.com/docs/vue/?utm_source=DemoApp&utm_medium=MDBVue"> MDBootstrap.com</a>
+      </p>
+    </mdb-footer>
+    <!-- <img src="./assets/logo-mdb-vue-small.png">
+    <router-view></router-view> -->
   </div>
 </template>
 
 <script>
-import { mdbNavbar, mdbNavItem, mdbNavbarNav, mdbNavbarToggler, mdbNavbarBrand, mdbFooter } from 'mdbvue';
+import { mdbNavbar, mdbNavItem, mdbNavbarNav, mdbNavbarToggler, mdbNavbarBrand, mdbFooter, mdbIcon } from 'mdbvue';
 
 export default {
   name: 'App',
@@ -16,7 +100,8 @@ export default {
     mdbNavbarNav,
     mdbNavbarToggler,
     mdbNavbarBrand,
-    mdbFooter
+    mdbFooter,
+    mdbIcon
   }
 }
 </script>
