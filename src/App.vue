70429d87272a4cac7bf7522a43d1d2f945fcd455
usuki@go-next.co.jp <f313ed@gmail.com>
2019-06-18 19:17:56 +0900

    template

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,73 @@
 <template>
   <div id="app">
     <img src="./assets/logo-mdb-vue-small.png">
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import { mdbNavbar, mdbNavItem, mdbNavbarNav, mdbNavbarToggler, mdbNavbarBrand, mdbFooter } from 'mdbvue';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    mdbNavbar,
+    mdbNavItem,
+    mdbNavbarNav,
+    mdbNavbarToggler,
+    mdbNavbarBrand,
+    mdbFooter
+  }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 100px;
+} */
+
+.flyout {
+	display:flex;
+	flex-direction: column;
+	min-height:100vh;
+	justify-content: space-between;
 }
+.active{
+  background-color: rgba(255, 255, 255, 0.1);
+}
+.demo-section {
+  padding: 20px 0;
+}
+.demo-section > section {
+  border: 1px solid #e0e0e0;
+  padding: 15px;
+}
+.demo-section > h4 {
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+.demo-title {
+  color: #9e9e9e;
+  font-weight: 700;
+  margin-bottom: 0;
+  padding-left: 15px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease-out;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+
 </style>
